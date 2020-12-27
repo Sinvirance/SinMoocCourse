10bbf50d76703ef3972ee90b5486e523e6b38449
sin <sinvirance@gmail.com>
2020-12-27 22:16:38 +0800

    1. 将system注册到Eureka中

--- system/src/main/java/top/course/system/SystemApplication.java
+++ system/src/main/java/top/course/system/SystemApplication.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- import org.springframework.core.env.Environment;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class SystemApplication {
 
     // 指定类初始化日志对象
