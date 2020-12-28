607178ab9d4b014149d2af161f703d3f96082476
sin <sinvirance@gmail.com>
2020-12-29 00:38:48 +0800

    1. Example使用示例 使用Example可以快速完成单表的增删改查，熟练掌握后对代码开发效率将有一个质的飞跃 2. 修改了公共模块日志配置的错误

--- server/src/main/java/top/course/server/service/TestService.java
+++ server/src/main/java/top/course/server/service/TestService.java
@@ -2,9 +2,12 @@ package top.course.server.service;
 
 
 import org.springframework.stereotype.Service;
+import top.course.server.domain.Test;
+import top.course.server.domain.TestExample;
 import top.course.server.mapper.TestMapper;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author: Sinvirance
@@ -17,4 +20,12 @@ public class TestService {
 
     @Resource
     private TestMapper testMapper;
+
+    public List<Test> list() {
+        TestExample testExample = new TestExample();
+        // 相当于: select id, name from test WHERE ( id = ? ) order by id desc
+        testExample.createCriteria().andIdEqualTo("1");
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
+    }
 }
