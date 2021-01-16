222217eb6db149d05b4b5912e21c36cc513850b4
sin <sinvirance@gmail.com>
2021-01-17 00:38:54 +0800

    1. 增加了business 模块，并使其初始化启动成功

---
+++ business/src/main/java/top/course/business/controller/TestController.java
@@ -0,0 +1,27 @@
+package top.course.business.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.course.server.domain.Test;
+import top.course.server.service.TestService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author: Sinvirance
+ * @Date: 2020/12/27 19:30
+ * @Description: 测试 Spring Boot 项目
+ */
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")
+    public List<Test> test() {
+        return testService.list();
+    }
+}
