10f64f7823a85ada9f33470874515b41921fccd9
sin <sinvirance@gmail.com>
2021-01-23 23:11:23 +0800

    1. 集成sweetalert2-一个响应式的JavaScript弹窗，删除时弹出确认框

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -178,12 +178,30 @@
        */
       del(id) {
         let _this = this;
-        // 使用 restful 风格传递要删除的id
-        _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/" + id).then((response) =>{
-          console.log("删除大章列表结果:", response);
-          let resp = response.data;
-          if (resp.success) {
-            _this.list(1);
+
+        Swal.fire({
+          title: '确认删除',
+          text: "删除后不可恢复，确认删除",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: '立即删除!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // 使用 restful 风格传递要删除的id
+            _this.$ajax.delete("http://127.0.0.1:9000/business/admin/chapter/delete/" + id).then((response) =>{
+              console.log("删除大章列表结果:", response);
+              let resp = response.data;
+              if (resp.success) {
+                _this.list(1);
+                Swal.fire(
+                        '删除成功',
+                        '点击返回!',
+                        'success'
+                )
+              }
+            })
           }
         })
       }
