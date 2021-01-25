990b43ef89522b53f291202f6222e1582a0ad5cd
sin <sinvirance@gmail.com>
2021-01-25 17:38:27 +0800

    1. 增加generator模块，用于代码生成 2. 集成模板引擎freemarker

---
+++ generator/src/main/java/top/course/generator/test/TestUtil.java
@@ -0,0 +1,44 @@
+package top.course.generator.test;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @Author: Sinvirance
+ * @Date: 2021/1/25 17:17
+ * @Description: TODO
+ */
+
+public class TestUtil {
+
+    /*
+     * ftlPath:模板文件路径
+     * toPath: 文件生成路径
+     */
+    static String ftlPath = "generator\\src\\main\\java\\top\\course\\generator\\test\\";
+    static String toPath = "generator\\src\\main\\java\\top\\course\\generator\\test\\";
+
+    public static void main(String[] args) throws IOException, TemplateException {
+        /*
+         * 根据freeMarker版本读取配置并针对配置和模板文件ftl
+         */
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File(ftlPath));
+        cfg.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
+        Template temp = cfg.getTemplate("test.ftl");
+
+
+        FileWriter fw = new FileWriter(toPath + "Test.java");
+        BufferedWriter bw = new BufferedWriter(fw);
+        temp.process(null, bw);
+        bw.flush();
+        fw.close();
+    }
+}
---
+++ generator/src/main/java/top/course/generator/test/test.ftl
@@ -0,0 +1,14 @@
+package top.course.generator.test;
+
+/**
+* @Author: Sinvirance
+* @Date: 2021/1/25 17:17
+* @Description: TODO
+*/
+
+public class Test {
+    /**
+    * ID
+    */
+    private String id;
+}
