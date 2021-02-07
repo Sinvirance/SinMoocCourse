bb6968f7710209c5060ded97ec82f3cec52ef28c
sin <sinvirance@gmail.com>
2021-02-08 00:48:45 +0800

    1. 文件上传功能开发：增加上传文件类型的判断

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -92,7 +92,7 @@
                     <i class="ace-icon fa fa-upload"></i>
                     上传头像
                   </button>
-                  <input class="hidden" type="file" v-on:change="uploadImage()" id="file-upload-input">
+                  <input class="hidden" type="file" ref="file" v-on:change="uploadImage()" id="file-upload-input">
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-5">
                       <!-- img-responsive: boostrap内置样式，图片自适应 -->
@@ -250,9 +250,28 @@
         let _this = this;
         /* FormData 接口提供了一种表示表单数据的键值对 key/value 的构造方式 */
         let formData = new window.FormData;
+        /* _this.$refs调用Dom节点元素值 */
+        let file = _this.$refs.file.files[0];
+
+        /* 判断文件格式 */
+        let suffixs = ["jpg", "jpeg", "png"];
+        let fileName = file.name;
+        let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
+        let validateSuffix = false;
+        for (let i = 0; i < suffixs.length; i++) {
+          if (suffixs[i].toLowerCase() === suffix) {
+            validateSuffix = true;
+            break;
+          }
+        }
+        if (!validateSuffix) {
+          Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(" "));
+          return;
+        }
+
         /* FormData 接口的append() 方法 会添加一个新值到 FormData 对象内的一个已存在的键中，如果键不存在则会添加该键。当该键有值时，进行追加而不是覆盖 */
         /* key：files 必须与后端名称一致*/
-        formData.append("file", document.querySelector("#file-upload-input").files[0]);
+        formData.append("file", file);
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + "/file/admin/upload", formData).then((response) =>{
           Loading.show();
