a3362958c9de7702b69405a54d65dc7a03f43b4d
sin <sinvirance@gmail.com>
2021-02-09 02:22:05 +0800

    1. 文件上传功能开发：上传文件时保存文件记录

--- file/src/main/java/top/course/file/controller/admin/UploadController.java
+++ file/src/main/java/top/course/file/controller/admin/UploadController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import top.course.server.dto.FileDto;
 import top.course.server.dto.ResponseDto;
+import top.course.server.service.FileService;
 import top.course.server.util.UUIDUtil;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 
@@ -34,6 +37,9 @@ public class UploadController {
     @Value("${file.domain}")
     private String FILE_DOMAIN;
 
+    @Resource
+    private FileService fileService;
+
     /**
      * 讲师头像文件上传
      * @param file 上传的文件
@@ -42,20 +48,34 @@ public class UploadController {
     @PostMapping("upload")
     public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
         LOG.info("文件上传开始：{}", file);
-        String filename = file.getOriginalFilename();
-        LOG.info(filename);
+        String fileName = file.getOriginalFilename();
+        LOG.info(fileName);
         LOG.info(String.valueOf(file.getSize()));
 
         /* 保存文件 */
         String key = UUIDUtil.getShortUUID();
-        String fullPath = FILE_PATH + "teacher/" + key + "-" + filename;
+        /* 文件格式 */
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        /* 相对路径 */
+        String path = "teacher/" + key + "." + suffix;
+        String fullPath = FILE_PATH + path;
         File dest = new File(fullPath);
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());
 
+        /* 保存文件记录 */
+        LOG.info("保存文件记录开始");
+        FileDto fileDto = new FileDto();
+        fileDto.setPath(path);
+        fileDto.setName(fileName);
+        fileDto.setSize(Math.toIntExact(file.getSize()));
+        fileDto.setSuffix(suffix);
+        fileDto.setUse("");
+        fileService.save(fileDto);
+
         ResponseDto responseDto = new ResponseDto();
         /* 将图片位置返回前端 */
-        responseDto.setContent(FILE_DOMAIN + "f/teacher/" + key + "-" + filename);
+        responseDto.setContent(FILE_DOMAIN + path);
         return responseDto;
     }
 
