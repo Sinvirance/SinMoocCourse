c24c4025cebd2fea4a11d3f4e20532c90322e58d
sin <sinvirance@gmail.com>
2021-02-09 03:58:58 +0800

    1. 文件上传功能开发：自动获取视频时长

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -97,7 +97,7 @@
                         v-bind:after-upload="afterUpload"></file>
                   <div v-show="section.video" class="row">
                     <div class="col-md-9">
-                      <video v-bind:src="section.video" controls="controls"></video>
+                      <video v-bind:src="section.video" id="video" controls="controls"></video>
                     </div>
                   </div>
                 </div>
@@ -270,7 +270,18 @@
         let _this = this;
         let video = resp.content.path;
         _this.section.video = video;
-      }
+        _this.getTime();
+      },
+
+
+      /**
+       * 获取时长
+       */
+      getTime() {
+        let _this = this;
+        let ele = document.getElementById("video");
+        _this.section.time = parseInt(ele.duration, 10);
+      },
     }
   }
 </script>
