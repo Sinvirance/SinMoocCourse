b6ed25560207f8dea1f4df76846cb101a2aded1c
sin <sinvirance@gmail.com>
2021-02-10 00:30:35 +0800

    1. 大文件断点续传与极速秒传：分片合并测试成功

--- file/src/main/java/top/course/file/controller/admin/UploadController.java
+++ file/src/main/java/top/course/file/controller/admin/UploadController.java
@@ -3,10 +3,7 @@ package top.course.file.controller.admin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import top.course.server.dto.FileDto;
 import top.course.server.dto.ResponseDto;
@@ -16,6 +13,8 @@ import top.course.server.util.UUIDUtil;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -88,6 +87,48 @@ public class UploadController {
         /* 将图片位置返回前端 */
         fileDto.setPath(FILE_DOMAIN + path);
         responseDto.setContent(fileDto);
+        return responseDto;
+    }
+
+
+    /**
+     * 测试：已有的分片追加合并
+     * @return 统一返回响应对象
+     */
+    @GetMapping("/merge")
+    public ResponseDto merge() throws Exception {
+        File newFile = new File(FILE_PATH + "/course/sinmooc.mp4");
+        FileOutputStream outputStream = new FileOutputStream(newFile, true);//文件追加写入
+        FileInputStream fileInputStream = null;//分片文件
+        byte[] byt = new byte[10 * 1024 * 1024];
+        int len;
+
+        try {
+            // 读取第一个分片
+            fileInputStream = new FileInputStream(new File(FILE_PATH + "/course/h6m1Ckhe.blob"));
+            while ((len = fileInputStream.read(byt)) != -1) {
+                outputStream.write(byt, 0, len);
+            }
+
+            // 读取第二个分片
+            fileInputStream = new FileInputStream(new File(FILE_PATH + "/course/PMqbK87u.blob"));
+            while ((len = fileInputStream.read(byt)) != -1) {
+                outputStream.write(byt, 0, len);
+            }
+        } catch (IOException e) {
+            LOG.error("分片合并异常", e);
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+                outputStream.close();
+                LOG.info("IO流关闭");
+            } catch (Exception e) {
+                LOG.error("IO流关闭", e);
+            }
+        }
+        ResponseDto responseDto = new ResponseDto();
         return responseDto;
     }
 
