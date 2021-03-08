834fb4b1320e620d2c3c9afc949539bf13727a3a
sin <sinvirance@gmail.com>
2021-03-09 03:45:57 +0800

    1. 用户管理与登录：增加记住登录信息功能，安全加固，本地缓存保存密码密文

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -105,9 +105,15 @@
       login(){
         let _this = this;
 
-        let passwordShow = _this.user.password;
-        /* 登陆时加密 */
-        _this.user.password = hex_md5(_this.user.password + KEY);
+        console.log(_this.user.password);
+        let md5 = hex_md5(_this.user.password);
+        console.log(md5);
+        let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER) || {};
+        if (md5 !== rememberUser.md5) {
+          _this.user.password = hex_md5(_this.user.password + KEY);
+          console.log(_this.user.password);
+        }
+
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then(
             (response) => {
@@ -120,10 +126,14 @@
 
                 // 判断“记住我”
                 if (_this.remember) {
+                  // 原：这里需要保存密码明文，否则登录时又会再加一层密
+                  // 新：这里保存密码密文，并保存密文md5，用于检测密码是否被重新输入过
+                  let md5 = hex_md5(_this.user.password);
                   // 如果勾选记住我，则将用户名密码保存到本地缓存，这里需要保存密码明文，否则登录时又会再加一层密
                   LocalStorage.set(LOCAL_KEY_REMEMBER_USER, {
                     loginName: loginUser.loginName,
-                    password: passwordShow
+                    password: _this.user.password,
+                    md5: md5,
                   });
                 } else {
                   // 没有勾选“记住我”时，要把本地缓存清空，否则按照mounted的逻辑，下次打开时会自动显示用户名密码
