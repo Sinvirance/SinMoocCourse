a6e312022b27e569628ca2544ce056e23c2b408b
sin <sinvirance@gmail.com>
2021-03-26 01:01:39 +0800

    1. 网站开发: 集成album模板

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+/* process.env: 会返回包含用户环境的对象 */
+console.log("前台环境：" + process.env.NODE_ENV);
