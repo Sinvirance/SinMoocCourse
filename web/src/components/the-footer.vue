cc42086069cf34ba7e5830e56f16b3b141000051
sin <sinvirance@gmail.com>
2021-03-26 04:35:15 +0800

    1. 网站首页开发: 底部文案修改

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,11 +1,28 @@
 <template>
-  <footer class="text-muted">
+  <footer class="texfot-muted">
     <div class="container">
-      <p class="float-right">
-        <a href="#">Back to top</a>
-      </p>
-      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p>New to Bootstrap? <a href="https://v4.bootcss.com/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+      <p>SinMooc 在线视频课程系统，程序员的梦工厂，致力于全栈技术推广</p>
+      <div class="footer-link">
+        <a href="#" title="回到顶部">回到顶部</a>
+        <a href="/about/us" target="_blank" title="关于我们">关于我们</a>
+        <a href="/about/contact" target="_blank" title="联系我们">联系我们</a>
+        <a href="/about/recruit" target="_blank" title="讲师招募">讲师招募</a>
+        <a href="/help" target="_blank" title="帮助中心">帮助中心</a>
+        <a href="/user/feedback" target="_blank" title="意见反馈">意见反馈</a>
+        <a href="/about/friendly" target="_blank" title="友情链接">友情链接</a>
+      </div>
+      <div class="footer-copyright">
+        <p>Copyright&nbsp;<img src="" draggable="false" alt="©">
+          &nbsp;2021&nbsp;sinmooc.com&nbsp;&nbsp;
+          <a rel="nofollow" href="https://beian.miit.gov.cn/" style="color: rgb(90, 107, 123);font-size:13px;" target="_blank">京ICP备 xxxxxxxxx号-xx</a>
+          <a href="http://www.beian.gov.cn/portal/index.do"
+             rel="nofollow" target="_blank"
+             style="color: rgb(90, 107, 123);font-size:13px;margin:0 5px !important;">
+            <i class="beian"></i>京公网安备xxxxxxxxxxxxxxxx号
+          </a>
+        </p>
+      </div>
+
     </div>
   </footer>
 </template>
@@ -15,3 +32,17 @@
     name: 'TheFooter',
   }
 </script>
+
+<style scoped>
+  .footer-link a {
+   display: inline-block;
+   font-size: 15px;
+   line-height: 28px;
+   color: #545C63;
+   margin-right: 24px;
+   font-weight: 400;
+ }
+  p {
+    color: #545C63;
+  }
+</style>
