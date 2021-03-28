326e2f0393de6ed50d78ecb329bf2ab17941aab9
sin <sinvirance@gmail.com>
2021-03-29 02:58:04 +0800

    1. 网站首页开发: 增加课程card组件the-course

---
+++ web/src/components/the-course.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card mb-4 shadow-sm course">
+    <img class="img-fluid" v-bind:src="course.image">
+    <div class="card-body">
+      <h4 class="">{{course.name}}</h4>
+      <p class="card-text">{{course.summary}}</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="btn-group">
+          <button type="button" class="btn btn-sm btn-outline-secondary">课程详情</button>
+        </div>
+        <div class="text-muted">
+          <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{course.price}}</span>&nbsp;
+          <span class="badge badge-info"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'the-course',
+    props: {
+      course: {}
+    },
+    data: function () {
+      return {}
+    },
+  }
+</script>
+
+<style>
+  .course h4 {
+    font-size: 1.25rem;
+    margin: 15px 0;
+  }
+
+  .course .text-muted .badge {
+    font-size: 1rem;
+  }
+
+  /* 设置summary只显示两行剩下的省略号代替*/
+  .card-text {
+    text-overflow:ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+</style>
--- web/src/view/index.vue
+++ web/src/view/index.vue
@@ -17,46 +17,15 @@
         <div class="title1">最新上线</div>
         <div class="row">
           <div v-for="o in news" class="col-md-4">
-            <div class="card mb-4 shadow-sm course">
-              <img class="img-fluid" v-bind:src="o.image">
-              <div class="card-body">
-                <h4 class="">{{o.name}}</h4>
-                <p class="card-text">{{o.summary}}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">课程详情</button>
-                  </div>
-                  <div class="text-muted">
-                    <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.price}}</span>&nbsp;
-                    <span class="badge badge-info"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
+            <the-course v-bind:course="o"></the-course>
           </div>
         </div>
 
         <hr style="FILTER: alpha(opacity=100,finishopacity=0,style=2)" width="80%" color=#987cb9 SIZE=10>
-        <div class="title1">近期热门</div>
+        <div class="title2">近期热门</div>
         <div class="row">
-
-          <div v-for="r in hots" class="col-md-4">
-            <div class="card mb-4 shadow-sm course">
-              <img class="img-fluid" v-bind:src="r.image">
-              <div class="card-body">
-                <h4 class="">{{r.name}}</h4>
-                <p class="card-text">{{r.summary}}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">课程详情</button>
-                  </div>
-                  <div class="text-muted">
-                    <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{r.price}}</span>&nbsp;
-                    <span class="badge badge-info"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div v-for="h in hots" class="col-md-4">
+            <the-course v-bind:course="h"></the-course>
           </div>
         </div>
 
@@ -67,8 +36,10 @@
 </template>
 
 <script>
+import TheCourse from "../components/the-course.vue";
 export default {
   name: "index",
+  components:{TheCourse},
   data: function () {
     return {
       news: [],
@@ -118,15 +89,6 @@ export default {
 
 <!-- scoped:只应用当前组件，防止相互污染 -->
 <style scoped>
-
-/* 设置summary只显示两行剩下的省略号代替*/
-.card-text {
-  text-overflow:ellipsis;
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-}
 .title1{
   margin-bottom: 2rem;
   color: #fafafa;
@@ -142,12 +104,4 @@ export default {
   font-size: 2rem;
 }
 
-.course h4 {
-  font-size: 1.25rem;
-  margin: 15px 0;
-}
-
-.course .text-muted .badge{
-  font-size: 1rem;
-}
 </style>
