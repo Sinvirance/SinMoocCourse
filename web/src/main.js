0049a201205c2e0639d5751f8c4983df0ce67792
sin <sinvirance@gmail.com>
2021-03-29 03:17:37 +0800

    1. 网站首页开发: 课程card组件增加等级显示 2. 增加过滤器filter.js

--- web/src/main.js
+++ web/src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './app.vue'
 import router from "./router";
 import axios from "axios";
+import filter from "./filter/filter"
 
 Vue.config.productionTip = false;
 /* 每一个vue组件都是Vue的实例，所以组件内this可以拿到Vue.prototype上添加的属性和方法 */
 Vue.prototype.$ajax = axios;
+
+/**
+ * 全局过滤器
+ */
+Object.keys(filter).forEach(key => {
+  /* Vue.filter()：把Array的某些元素过滤掉，然后返回剩下的元素,这里是将key去掉，留value  */
+  Vue.filter(key, filter[key])
+});
 
 new Vue({
   router,
