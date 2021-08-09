d33ca6e39945a7cfec497118aa1d2c99d0517763
sinvirance <Sinvirance@outlook.com>
2021-08-09 17:10:11 +0800

    1. 会员登录注册功能开发: 短信管理只能查看

--- admin/src/views/admin/sms.vue
+++ admin/src/views/admin/sms.vue
@@ -2,11 +2,6 @@
   <div>
     <!--新增短信验证码和刷新按钮-->
     <p>
-      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-edit"></i>
-        新增
-      </button>
-      &nbsp;
       <!--点击触发事件查询list()-->
       <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh "></i>
@@ -24,7 +19,6 @@
         <th>用途</th>
         <th>生成时间</th>
         <th>状态</th>
-        <th>操作</th>
       </tr>
       </thead>
       <tbody>
@@ -35,76 +29,13 @@
         <td>{{SMS_USE | optionKV(sms.use)}}</td>
         <td>{{sms.at}}</td>
         <td>{{SMS_STATUS | optionKV(sms.status)}}</td>
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(sms)" class="btn btn-xs btn-info">
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-            <button v-on:click="del(sms.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
-          </div>
-        </td>
-      </tr>
+			</tr>
       </tbody>
     </table>
 
     <!-- 分页组件显示 -->
     <pagination ref="pagination" v-bind:list="list"/>
 
-    <!--新增或编辑短信验证码功能表单-->
-    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">短信验证码编辑</h4>
-          </div>
-          <div class="modal-body">
-            <form class="form-horizontal">
-              <div class="form-group">
-                <label class="col-md-2 control-label">手机号</label>
-                <div class="col-sm-9">
-                  <input v-model="sms.mobile" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-md-2 control-label">验证码</label>
-                <div class="col-sm-9">
-                  <input v-model="sms.code" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-md-2 control-label">用途</label>
-                <div class="col-sm-10">
-                  <select v-model="sms.use" class="form-control">
-                    <option v-for="o in SMS_USE" v-bind:value="o.key">{{o.value}}</option>
-                  </select>
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-md-2 control-label">生成时间</label>
-                <div class="col-sm-9">
-                  <input v-model="sms.at" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-md-2 control-label">状态</label>
-                <div class="col-sm-10">
-                  <select v-model="sms.status" class="form-control">
-                    <option v-for="o in SMS_STATUS" v-bind:value="o.key">{{o.value}}</option>
-                  </select>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -132,15 +63,6 @@
     methods: {
 
       /**
-       * 添加短信验证码功能,点击弹出表单
-       */
-      add() {
-        let _this = this;
-        _this.sms = {};
-        $("#form-modal").modal("show");
-      },
-
-      /**
        * 显示指定页码的短信验证码列表
        * @param page 前端传入查询的页码数
        */
@@ -158,67 +80,6 @@
         })
       },
 
-      /**
-       * 表单添加短信验证码数据后保存
-       */
-      save() {
-        let _this = this
-
-        /* 保存时校验 */
-        if (1 != 1
-          || !Validator.require(_this.sms.mobile, "手机号")
-          || !Validator.length(_this.sms.mobile, "手机号", 1, 50)
-          || !Validator.require(_this.sms.code, "验证码")
-          || !Validator.require(_this.sms.use, "用途")
-          || !Validator.require(_this.sms.at, "生成时间")
-          || !Validator.require(_this.sms.status, "状态")
-        ) {
-          return;
-        }
-
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/sms/save", _this.sms).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          if (resp.success) {
-            $("#form-modal").modal("hide");
-            _this.list(1);
-            Toast.success("保存成功！");
-          } else {
-            Toast.warning(resp.message)
-          }
-        })
-      },
-
-      /**
-       * 表单修改已有短信验证码数据
-       * @param sms
-       */
-      edit(sms) {
-        let _this = this;
-        _this.sms = $.extend({},sms);
-        $("#form-modal").modal("show");
-      },
-
-      /**
-       * 根据短信验证码id删除对应数据
-       * @param id
-       */
-      del(id) {
-        let _this = this;
-        Confirm.show("删除短信验证码数据后不可恢复，确认删除?", function () {
-          Loading.show();
-          // 使用 restful 风格传递要删除的id
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + "/business/admin/sms/delete/" + id).then((response) =>{
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          })
-        })
-      }
     }
   }
 </script>
