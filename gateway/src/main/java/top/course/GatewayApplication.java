2235f1ee264ad752bd76bed4e5b6bac57991115e
sinvirance <Sinvirance@outlook.com>
2021-08-19 16:18:34 +0800

    1. 将gateway, file, business，system 4个模块注册中心改为nacos

--- gateway/src/main/java/top/course/GatewayApplication.java
+++ gateway/src/main/java/top/course/GatewayApplication.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,7 +14,8 @@ import org.springframework.web.util.pattern.PathPatternParser;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+/* @EnableEurekaClient */
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     // 指定类初始化日志对象
--- system/src/main/java/top/course/system/config/SystemApplication.java
+++ system/src/main/java/top/course/system/config/SystemApplication.java
@@ -5,13 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+/* @EnableEurekaClient */
+@EnableDiscoveryClient
 @ComponentScan("top.course")
 @MapperScan("top.course.server.mapper")
 public class SystemApplication {
